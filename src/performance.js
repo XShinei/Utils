4d57ff5d25f3eabdd1699a8c1f089a4f31ee130b
XShinei <497449921@qq.com>
2018-11-05 22:30:59 +0800

    debounce

---
+++ src/performance.js
@@ -0,0 +1,83 @@
+/**
+ *  base
+ */
+function debounce(fn, delay) {
+    let timer = null;
+
+    return function() {
+        const context = this;
+        const args = arguments;
+
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+
+        timer = setTimeout(() => {
+            fn.apply(context, args);
+        }, delay);
+    };
+}
+
+/**
+ *  immediate
+ */
+function debounce(fn, delay, immediate) {
+    let timer = null;
+
+    return function() {
+        const context = this;
+        const args = arguments;
+
+        timer && clearTimeout(timer);
+
+        if(immediate) {
+            if (timer) {
+                timer = setTimeout(() => {
+                    timer = null;
+                }, delay);
+            }
+            else {
+                fn.apply(context, args);
+            }
+        }
+        else {
+            timer = setTimeout(() => {
+                fn.apply(context, args);
+            }, delay);
+        }
+    };
+}
+
+/**
+ *  return
+ */
+function debounce(fn, delay, immediate) {
+    let timer = null;
+
+    return function() {
+        const context = this;
+        const args = arguments;
+        let result = undefined;
+
+        timer && clearTimeout(timer);
+
+        if (immediate) {
+            if (timer) {
+                timer = setTimeout(() => {
+                    timer = null;
+                }, delay);
+            }
+            else {
+                result = fn.apply(context, args);
+            }
+        }
+        else {
+            timer = setTimeout(() => {
+                fn.apply(context, args);
+            }, delay);
+        }
+
+        return result;
+    };
+}
