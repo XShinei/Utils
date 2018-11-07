973a12328a98e45b58f02d9f19dcca8f72fdceea
XShinei <497449921@qq.com>
2018-11-07 19:18:47 +0800

    finish return Promise

--- src/performance.js
+++ src/performance.js
@@ -85,3 +85,30 @@ function debounce(fn, delay, immediate) {
 /**
  *  return Promise
  */
+function debounce(fn, delay, immediate) {
+    let timer = null;
+
+    return function() {
+        const context = this;
+        const args = arguments;
+
+        return new Promise((resolve, reject) => {
+            timer && clearTimeout(timer);
+
+            if (immediate) {
+                const doNow = !timer;
+
+                timer = setTimeout(() => {
+                    timer = null;
+                }, delay);
+
+                doNow && resolve(fn.apply(context, args));
+            }
+            else {
+                timer = setTimeout(() => {
+                    resolve(fn.apply(context, args));
+                }, delay);
+            }
+        });
+    };
+}
